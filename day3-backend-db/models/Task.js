539af281f1e1f70766e3c958e1fdace5a6e6640d
battlefield-war <naikshrihari8421@gmail.com>
2026-01-31 12:51:23 +0530

    feat: implement update task route in taskRoutes.js

--- day3-backend-db/models/Task.js
+++ day3-backend-db/models/Task.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
+  title: String,
+  completed: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model("Task", taskSchema);
